d28c621ad21d1dbcc94f11cbdf78fa04d7cf52b2
Ilya Radchenko <ilya@burstcreations.com>
2016-06-10 13:00:49 -0400

    Update Shim Version (#6)
    Was causing the following https://github.com/zloirock/core-js/issues/208#issuecomment-224945389 in phantom

--- blueprints/ember-cli-es5-shim/index.js
+++ blueprints/ember-cli-es5-shim/index.js
@@ -4,6 +4,6 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('es5-shim', '^4.0.5');
+    return this.addBowerPackageToProject('es5-shim', '^4.5.8');
   }
 };
